208fde86362b4739a2abe3987a7109e66f69522e
Poterman <46090842+Poterman@users.noreply.github.com>
2019-09-24 10:11:00 +0300

    Add files via upload

---
+++ centos_prep.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+################################################
+#Created By: Poterman
+#Date: 23/09/19
+#Purpose: Automatic CentOS First Configuration
+################################################
+
+#Updating OS
+sudo yum -y update
+
+#Installing Epel-Release Repository
+sudo yum -y install epel-release
+
+#Installing Tools
+sudo yum -y install vim git 
+
+#Installing GUI - Optional
+printf "%s\n" "Do you wish to install GNOME Desktop?"
+printf "%s\n" "Type [Y]es / [N]o"
+read answer
+if [[ $answer == "Y" || $answer == "y" ]]; then
+  echo "Installing GNOME Desktop"
+  sudo yum groupinstall -y "GNOME Desktop"
+  systemctl set-default graphical.target
+elif [[ $answer == "N" || $answer == "n" ]]; then
+  echo "You chose NOT to install GNOME Desktop"
+fi
